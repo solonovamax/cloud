bd261a0dcae635a3858421909c57460e5dbf6b01
jmp <jasonpenilla2@me.com>
2021-04-20 00:28:13 -0700

    sponge: Add some argument types and an example/test plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -40,6 +40,7 @@ setupExampleModule("example-bukkit")
 setupExampleModule("example-bungee")
 setupExampleModule("example-jda")
 setupExampleModule("example-velocity")
+setupExampleModule("example-sponge")
 
 fun setupIrcModule(name: String) =
         setupSubproject(name, file("cloud-irc/$name"))
